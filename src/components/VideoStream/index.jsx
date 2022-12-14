50ffd8de94f9af740452e3299d71f1386303a6fd
markleylopes <walissonmarkley@gmail.com>
2022-12-14 02:56:22 -0300

    ajuste de importação

--- src/components/VideoStream/index.jsx
+++ src/components/VideoStream/index.jsx
@@ -1,6 +1,6 @@
 
 import PropTypes from "prop-types"
-import React, { memo } from 'react'
+import { memo } from "react"
 
 const styles = {
 	wrapper: { display: "inline-block", position: "relative", width: "50%", height: "400px", overflow: "hidden" },
