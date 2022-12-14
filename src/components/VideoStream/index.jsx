6b09222d29451624f658fad0f9ceb5a4ac0b8620
markleylopes <walissonmarkley@gmail.com>
2022-12-14 02:55:40 -0300

    Adição do memo ao componente de vídeo

--- src/components/VideoStream/index.jsx
+++ src/components/VideoStream/index.jsx
@@ -1,5 +1,6 @@
 
 import PropTypes from "prop-types"
+import React, { memo } from 'react'
 
 const styles = {
 	wrapper: { display: "inline-block", position: "relative", width: "50%", height: "400px", overflow: "hidden" },
@@ -13,7 +14,8 @@ const styles = {
 	}
 }
 
-export function VideoStream({ stream, isRemote }) {
+// eslint-disable-next-line react/display-name
+const VideoStream = memo(({ stream, isRemote }) => {
 	const getStream = (audio) => {
 		if (audio && stream) {
 			audio.srcObject = stream
@@ -42,9 +44,11 @@ export function VideoStream({ stream, isRemote }) {
 			/>
 		</div>
 	)
-}
+})
 
 VideoStream.propTypes = {
 	stream: PropTypes.oneOfType([undefined, PropTypes.srcObject, PropTypes.stream]),
 	isRemote: PropTypes.bool
 }
+
+export { VideoStream }
